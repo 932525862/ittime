6fe948feba0ddac0cbf7eb825a5dee5568e11e5b
Ahadjon Mahkamov <94121122+Mahkamov9@users.noreply.github.com>
2026-06-15 03:45:13 +0500

    Add new lessons to lessonsData.js
    polar4darsqo'shildi

--- src/data/lessonsData.js
+++ src/data/lessonsData.js
@@ -2,6 +2,11 @@
 export const lessons = [
   { id: 1, title: 'Dasturlashni 0 dan Boshlash | Internet va Veb-saytlar Qanday Ishlaydi? (1-Dars)', youtubeId: 'QZDtwoek14Y' },
   { id: 2, title: 'HTML Nima? HTML Asoslari va Teglar', youtubeId: '6c6o9q3UB7Y' },
+  { id: 7, title: '1-Dars: Loyihani sozlash va Navbar qismi', youtubeId: 'coYRq7x-fL4' },
+  { id: 8, title: '2-Dars: Hero Section (Asosiy Banner) dizayni', youtubeId: 'kmLS5ZzoLdo' },
+  { id: 9, title: '3-Dars: Features Section (Xizmatlar va Afzalliklar kartalari)', youtubeId: 'zRft3gLllRw' },
+  { id: 10, title: 'Saytni Yakunlash, Effektlar va Deploy', youtubeId: '5vRkAGXefwA' },
+  
  
 ];
 
