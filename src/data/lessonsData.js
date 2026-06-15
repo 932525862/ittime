522a84f65f862834e19282e52d160d0b28b66966
Ahadjon Mahkamov <94121122+Mahkamov9@users.noreply.github.com>
2026-06-15 11:45:33 +0500

    Update lessonsData.js

--- src/data/lessonsData.js
+++ src/data/lessonsData.js
@@ -2,10 +2,15 @@
 export const lessons = [
   { id: 1, title: 'Dasturlashni 0 dan Boshlash | Internet va Veb-saytlar Qanday Ishlaydi? (1-Dars)', youtubeId: 'QZDtwoek14Y' },
   { id: 2, title: 'HTML Nima? HTML Asoslari va Teglar', youtubeId: '6c6o9q3UB7Y' },
-  { id: 7, title: '1-Dars: Loyihani sozlash va Navbar qismi', youtubeId: 'coYRq7x-fL4' },
-  { id: 8, title: '2-Dars: Hero Section (Asosiy Banner) dizayni', youtubeId: 'kmLS5ZzoLdo' },
-  { id: 9, title: '3-Dars: Features Section (Xizmatlar va Afzalliklar kartalari)', youtubeId: 'zRft3gLllRw' },
-  { id: 10, title: 'Saytni Yakunlash, Effektlar va Deploy', youtubeId: '5vRkAGXefwA' },
+  { id: 3, title: '1-dars. React & Tailwind CSS: 5 kunda to`liq YouTube Clone yaratish', youtubeId: '8Gi0cv8oH6w' },
+  { id: 4, title: '2-dars. React & Tailwind CSS: 5 kunda to`liq YouTube Clone yaratish', youtubeId: '-7a3vJG9r3E' },
+  { id: 5, title: '3-dars. React & Tailwind CSS: 5 kunda to`liq YouTube Clone yaratish', youtubeId: '8o-sOXwqUHc' },
+  { id: 6, title: '4-dars. React & Tailwind CSS: 5 kunda to`liq YouTube Clone yaratish', youtubeId: 'IEdLVEEpfTU' },
+  { id: 7, title: '5-dars. React & Tailwind CSS: 5 kunda to`liq YouTube Clone yaratish', youtubeId: 'ZP2ZqaP3aJg' },
+  { id: 8, title: '1-Dars: Loyihani sozlash va Navbar qismi', youtubeId: 'coYRq7x-fL4' },
+  { id: 9, title: '2-Dars: Hero Section (Asosiy Banner) dizayni', youtubeId: 'kmLS5ZzoLdo' },
+  { id: 10, title: '3-Dars: Features Section (Xizmatlar va Afzalliklar kartalari)', youtubeId: 'zRft3gLllRw' },
+  { id: 11, title: 'Saytni Yakunlash, Effektlar va Deploy', youtubeId: '5vRkAGXefwA' },
   
  
 ];
